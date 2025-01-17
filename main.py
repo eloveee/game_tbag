67790077f638a466707992fa645b036bbdb70938
eloveee <lilylukan@hotmail.com>
2025-01-17 11:09:33 +0000

    commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from room import Room
+
+kitchen = Room("kitchen")
+ballroom = Room("ballroom")
+dining_hall = Room("dining hall")
+
+
+kitchen.set_description("A dank and dirty room buzzing with flies")
+
+
